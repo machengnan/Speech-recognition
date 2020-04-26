26b7a84d95307fc7d4d701cddf4fee6c5efd5320
SAIC_G7066 <2624079968@qq.com>
2020-04-26 10:43:32 +0800

    add file

---
+++ example/knn.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 24 09:48:56 2020
+
+@author: machengnan
+"""
+import time
+import os
+import IPython.display as ipd
+import matplotlib.pyplot as plt
+import librosa.display
+import librosa
+import numpy as np
+from dtw import dtw
+from numpy.linalg import norm
+from sklearn.neighbors import KNeighborsClassifier
+# D:\ProgramData\Anaconda3\lib\site-packages\librosa\util\decorators.py
+
+dirname = "train"
+files = [f for f in os.listdir(dirname) if not f.startswith('.')]
+start = time.time()
+# minval = 200
+distances = np.ones((len(files), len(files)))
+y = np.ones(len(files))
+
+for i in range(len(files)):
+    y1, sr1 = librosa.load(dirname+"/"+files[i])
+    mfcc1 = librosa.feature.mfcc(y1, sr1)
+    for j in range(len(files)):
+        y2, sr2 = librosa.load(dirname+"/"+files[j])
+        mfcc2 = librosa.feature.mfcc(y2, sr2)
+        dist, _, _, _ = dtw(mfcc1.T, mfcc2.T, dist=lambda x, y: norm(x - y, ord=1))
+#         print files[i], mfcc1.T[0][0], mfcc2.T[0][0], files[j], dist
+#         if dist < minval:
+#             minval = dist
+        distances[i,j] = dist
+    if i % 2 == 0:
+        y[i] = 0  # 'a'
+    else:
+        y[i] = 1  # 'b'
+
+end = time.time()
+print("Time used: {}s".format(end-start))
+label = ['a','b']
+classifier = KNeighborsClassifier(n_neighbors=3, metric='euclidean')
+classifier.fit(distances, y)
+
+y, sr = librosa.load('./test/farw0-b1-t.wav')
+mfcc = librosa.feature.mfcc(y, sr)
+distanceTest = []
+for i in range(len(files)):
+    y1, sr1 = librosa.load(dirname+"/"+files[i])
+    mfcc1 = librosa.feature.mfcc(y1, sr1)
+    dist, _, _, _ = dtw(mfcc.T, mfcc1.T, dist=lambda x, y: norm(x - y, ord=1))
+    distanceTest.append(dist)
+
+# print(distanceTest)
+pre = classifier.predict([distanceTest])[0]
+print("Predict audio is: '{}'".format(label[int(pre)]))
---
+++ example/mfcc_example.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 24 09:48:56 2020
+
+@author: machengnan
+"""
+
+import matplotlib.pyplot as plt
+import librosa.display
+import librosa
+#import IPython.display as ipd
+
+y1, sr1 = librosa.load('./sounds/10.wav')
+y2, sr2 = librosa.load('./sounds/78.wav')
+
+# plt.subplot(1, 2, 1)
+mfcc1 = librosa.feature.mfcc(y1, sr1)
+# librosa.display.specshow(mfcc1)
+# plt.subplot(1, 2, 2)
+mfcc2 = librosa.feature.mfcc(y2, sr2)
+# librosa.display.specshow(mfcc2)
+print(mfcc1.shape)
+print(mfcc2.shape)
+
+
+from dtw import dtw
+from numpy.linalg import norm
+dist, cost, acc_cost, path = dtw(mfcc1.T, mfcc2.T, dist=lambda x, y: norm(x - y, ord=1))
+print('Normalized distance between the two sounds:', dist)
+
+
+plt.imshow(cost.T, origin='lower', cmap=plt.cm.gray, interpolation='nearest')
+plt.plot(path[0], path[1], 'w')
+plt.xlim((-0.5, cost.shape[0]-0.5))
+plt.ylim((-0.5, cost.shape[1]-0.5))
+plt.show()
